4e4c701f7de937bc7f89597e62623141fcbc5f1d
Brian <wamwangimathenge@gmail.com>
2022-11-23 23:24:37 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(num1, num2) {
+    num1 = 2000;
+    num2 = 100;
+    return num1 / num2;
+}
+
+const square = (num3) => {
+    return num3 * num3
+}
+
+const add = (num4, num5) => {
+    return num4 + num5
+}
